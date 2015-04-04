230af9272bf80c66df240e417e99d7102118528a
Jay Bisa <jaybisa@Jay-Bisas-MacBook-Pro.local>
2015-04-04 13:42:46 -0400

    Social media links work.

--- js/controllers/wwydCtrl.js
+++ js/controllers/wwydCtrl.js
@@ -15,16 +15,32 @@ myApp.controller('WWYDCtrl', ['$scope', '$sce', '$resource', function($scope, $s
             $scope.videos = videos;
             var id = Math.floor(Math.random() * $scope.videos.length);
             $scope.title = $scope.videos[id].title;
-            //$scope.link = $sce.trustAsResourceUrl($scope.videos[id].link);
 
             var videoPlayer = document.getElementById('video-player');
             var iframe = document.createElement('iframe');
             iframe.class = "embed-responsive-item";
             iframe.src = $sce.trustAsResourceUrl($scope.videos[id].link);
             videoPlayer.appendChild(iframe);
-
-
         })
+
+        /**
+        $scope.indices = [];
+        $scope.randomIndices = [];
+        var videosLength = $scope.videos.length,
+            randomNumber = 0;
+
+        for (i = 0; i < videosLength; i++) {
+            $scope.indices.push(i);
+        }
+
+        while(videosLength--) {
+            randomNumber = Math.floor(Math.random() * (videosLength+1));
+            $scope.randomIndices.push($scope.indices[randomNumber]);
+            $scope.indices.splice(randomNumber, 1);
+        }
+         */
+
+
     }
 
     $scope.somethingElse = function() {
